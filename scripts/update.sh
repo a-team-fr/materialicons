ae999fe323fc70c0f0237e65571aec2603237a14
guillaume charbonnier <gcharbonnier>
2020-06-22 12:23:58 +0200

    Add list of Icons as a ListModel

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,3 +3,5 @@
 PROPERTIES=`mktemp`
 node convert.js codepoints > $PROPERTIES
 sed "/IconsLists/ r $PROPERTIES" template > ../MaterialIcons/MaterialIcons.qml
+node convertList.js codepoints > $PROPERTIES
+sed "/IconsLists/ r $PROPERTIES" templateList > ../MaterialIcons/ListMaterialIcons.qml
